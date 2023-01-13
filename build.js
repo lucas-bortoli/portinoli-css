b9c38b4f09089a9ca0e408cbb7b6209fc25cde81
Lucas Bortoli <lucasbortolisantos@gmail.com>
2023-01-13 09:24:49 -0300

    select, input[type="date"], input[type="time"], icons

--- build.js
+++ build.js
@@ -1,8 +1,9 @@
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-import { statSync, watch, writeFileSync } from "fs";
+import { readFileSync, statSync, watch, writeFileSync } from "fs";
 import chalk from "chalk";
 import sass from "sass";
+const { SassString } = sass;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -24,7 +25,21 @@ const build = () => {
   log(chalk.blue("Output") + ": " + outputFile);
 
   try {
-    const compiled = sass.compile(inputFile, { style: "compressed" });
+    const compiled = sass.compile(inputFile, {
+      style: "compressed",
+
+      functions: {
+        "svg-data-url($filename)": (args) => {
+          const filenameArg = args[0].assertString().text;
+
+          const resolved = join(__dirname, "src/assets/", filenameArg);
+
+          const svgContents = readFileSync(resolved, { encoding: "base64" });
+
+          return new SassString(`data:image/svg+xml;base64,${svgContents}`);
+        },
+      },
+    });
 
     // Escrever CSS resultante na pasta raiz da biblioteca
     writeFileSync(outputFile, compiled.css);
